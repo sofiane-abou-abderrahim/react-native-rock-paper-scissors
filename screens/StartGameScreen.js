fa26a39d9d8f38ef2ec5479d77f8413fac91045e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-02-22 14:57:25 +0100

    Styling the Text Input Element + Configuring the TextInput Field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -37,11 +37,13 @@ function StartGameScreen() {
     <View>
       {gameIsRunning ? (
         <View>
-          <Text>Enter Your Choice</Text>
+          <Text style={styles.text}>Enter Your Choice</Text>
           <TextInput
+            style={styles.textInput}
             placeholder="ROCK, PAPER or SCISSORS?"
             onChangeText={(text) => setEnteredValue(text)}
             value={enteredValue}
+            autoCorrect={false}
           />
           <PrimaryButton onPress={getPlayerChoice}>Confirm</PrimaryButton>
         </View>
@@ -55,3 +57,20 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+  },
+  textInput: {
+    height: 50,
+    width: 200,
+    fontSize: 14,
+    borderBottomColor: "#72063c",
+    borderBottomWidth: 2,
+    color: "#72063c",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
