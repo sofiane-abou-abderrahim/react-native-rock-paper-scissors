bcd802316124c195ac1311005134a5e07d56c8a1
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-02-22 16:21:53 +0100

    Implementing the Core Game Logic

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,6 +7,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 function StartGameScreen() {
   const [gameIsRunning, setGameIsRunning] = useState(false);
@@ -16,7 +19,6 @@ function StartGameScreen() {
     if (gameIsRunning) {
       return;
     }
-
     setGameIsRunning(true);
     console.log("Game is starting...");
   }
@@ -27,10 +29,40 @@ function StartGameScreen() {
       Alert.alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
       selection = DEFAULT_USER_CHOICE;
     }
-
-    console.log(selection);
-
+    console.log(`Player chose: ${selection}`);
     return selection;
+  };
+
+  const getComputerChoice = function () {
+    let selection;
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+      selection = ROCK;
+      console.log(`Computer chose: ${selection}`);
+      return selection;
+    } else if (randomValue < 0.67) {
+      selection = PAPER;
+      console.log(`Computer chose: ${selection}`);
+      return selection;
+    } else {
+      selection = SCISSORS;
+      console.log(`Computer chose: ${selection}`);
+      return selection;
+    }
+  };
+
+  const getWinner = function (cChoice, pChoice) {
+    if (cChoice === pChoice) {
+      return RESULT_DRAW;
+    } else if (
+      (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ) {
+      return RESULT_PLAYER_WINS;
+    } else {
+      return RESULT_COMPUTER_WINS;
+    }
   };
 
   return (
@@ -45,7 +77,16 @@ function StartGameScreen() {
             value={enteredValue}
             autoCorrect={false}
           />
-          <PrimaryButton onPress={getPlayerChoice}>Confirm</PrimaryButton>
+          <PrimaryButton
+            onPress={function () {
+              const playerChoice = getPlayerChoice();
+              const computerChoice = getComputerChoice();
+              const winner = getWinner(computerChoice, playerChoice);
+              console.log(`Result: ${winner}`);
+            }}
+          >
+            Confirm
+          </PrimaryButton>
         </View>
       ) : (
         <PrimaryButton color="#841584" onPress={startNewGameHandler}>
